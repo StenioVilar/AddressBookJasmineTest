0018d0c5db8e697d4ef04a92414f1f0ff4fff818
Stenio Vilar <stenio.vilar@engesoftware.com.br>
2019-01-15 10:53:53 -0200

    Fim do Exercício

--- spec/AddressBook.js
+++ spec/AddressBook.js
@@ -1,6 +1,7 @@
 function AddressBook() {
 	
 	this.contactList = [];
+	this.initialComplete = false;
 	
 }
 
@@ -34,3 +35,17 @@ AddressBook.prototype.deleteContact  = function (index) {
 	this.contactList.splice(index,1);
 	
 }
+
+AddressBook.prototype.getInitialContacts  = function (cb) {
+	
+	var self = this;
+	
+	setTimeout(function() {
+		self.initialComplete = true;
+		if(cb) {
+			return cb();
+		}
+	}, 3);
+	
+}
+
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,8 +1,16 @@
 describe('Address Book',function (){
 	
-	it('should ble able to add a contact', function () {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
+	var adressBook, 
+		thisContact;
+	
+	beforeEach(function () {
+		
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+		
+	});
+	
+	it('should ble able to add a contact', function () {	
 		
 			addressBook.addContact(thisContact);
 			
@@ -10,13 +18,32 @@ describe('Address Book',function (){
 	});
 	
 	it('should ble able to delete a contact', function () {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
 		
 			addressBook.addContact(thisContact);
 			addressBook.deleteContact(0);
 			
 			expect(addressBook.getContact(0)).not.toBeDefined();
+	});
+	
+});
+
+describe ('Async Adress Book', function() {
+	
+	var adressBook;
+	
+	beforeEach(function (done) {
+		
+		addressBook.getInitialContacts(function(){
+			done();
+		});
+		
+	});
+	
+	it('should grab initial contacts', function(done) {
+		
+		expect(addressBook.initialComplete).toBe(true);
+		done();
+		
 	});
 	
 });
